4b0f94ec9e1d8818eb0b288b49d03fff742bc220
Simon Heybrock <simon.heybrock@ess.eu>
2023-11-13 06:35:13 +0100

    Add more checks

--- src/notevennexus/validate.py
+++ src/notevennexus/validate.py
@@ -1,23 +1,30 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 from __future__ import annotations
+
 from dataclasses import dataclass
+
 from .tree import Dataset, Group
 
 
 @dataclass
 class Violation:
     name: str
-    description: str
+    description: str | None = None
 
     def format(self) -> str:
-        return f"{self.name}: {self.description}"
+        return (
+            f"{self.name} ({self.description})"
+            if self.description is not None
+            else self.name
+        )
 
 
 class Validator:
     def __init__(self, name: str, description: str) -> None:
         self.name = name
         self.description = description
+        self._count = 0
         self._violations: list[Violation] = []
 
     def applies_to(self, node: Dataset | Group) -> bool:
@@ -30,6 +37,7 @@ class Validator:
 
     def apply(self, tree: dict[str, Dataset | Group], node: Dataset | Group) -> None:
         if self.applies_to(node):
+            self._count += 1
             if (violation := self.validate(tree, node)) is not None:
                 self._violations.append(violation)
 
@@ -37,8 +45,11 @@ class Validator:
         return self._violations
 
     def report(self) -> str:
-        content = '\n'.join([v.format() for v in self.violations()])
-        return f"{self.name}: {self.description}\nViolations ({len(self._violations)}):\n{content}"
+        content = '\n    '.join([v.format() for v in self.violations()])
+        violations = (
+            f'Violations ({len(self._violations)}/{self._count}):\n    {content}'
+        )
+        return f"{self.name}: {self.description}\n{violations}"
 
 
 def validate(tree: dict[str, Dataset | Group], validators: list[Validator]) -> None:
--- src/notevennexus/validators.py
+++ src/notevennexus/validators.py
@@ -4,9 +4,9 @@ from .tree import Dataset, Group
 from .validate import Validator, Violation
 
 
-class has_NX_class_attr(Validator):
+class NX_class_attr_missing(Validator):
     def __init__(self) -> None:
-        super().__init__("has_NX_class_attr", "NX_class attribute is present")
+        super().__init__("NX_class_attr_missing", "NX_class attribute is missing")
 
     def applies_to(self, node: Dataset | Group) -> bool:
         return isinstance(node, Group)
@@ -15,12 +15,12 @@ class has_NX_class_attr(Validator):
         self, tree: dict[str, Dataset | Group], node: Dataset | Group
     ) -> Violation | None:
         if "NX_class" not in node.attrs:
-            return Violation(node.name, "NX_class attribute is missing")
+            return Violation(node.name)
 
 
-class depends_on_exists(Validator):
+class depends_on_target_missing(Validator):
     def __init__(self) -> None:
-        super().__init__("depends_on_exists", "depends_on target is present")
+        super().__init__("depends_on_target_missing", "depends_on target is missing")
 
     def applies_to(self, node: Dataset | Group) -> bool:
         return node.name.endswith('/depends_on') or "depends_on" in node.attrs
@@ -53,5 +53,122 @@ class legacy_nexus_class(Validator):
                 return Violation(node.name, f"NX_class {nx_class} is deprecated")
 
 
+class group_has_units(Validator):
+    def __init__(self) -> None:
+        super().__init__("group_has_units", "Group should not have units attribute")
+
+    def applies_to(self, node: Dataset | Group) -> bool:
+        return isinstance(node, Group)
+
+    def validate(
+        self, tree: dict[str, Dataset | Group], node: Dataset | Group
+    ) -> Violation | None:
+        if 'units' in node.attrs:
+            return Violation(node.name)
+
+
+class invalid_units(Validator):
+    def __init__(self) -> None:
+        super().__init__("invalid_units", "Invalid units")
+
+    def applies_to(self, node: Dataset | Group) -> bool:
+        return isinstance(node, Dataset) and 'units' in node.attrs
+
+    def validate(
+        self, tree: dict[str, Dataset | Group], node: Dataset | Group
+    ) -> Violation | None:
+        units = node.attrs['units']
+        if units.startswith('NX_'):
+            return Violation(node.name, f"Invalid units {units}")
+        invalid = ['hz']
+        if units in invalid:
+            return Violation(node.name, f"Invalid units {units}")
+
+
+class index_has_units(Validator):
+    def __init__(self) -> None:
+        super().__init__("index_has_units", "Index should not have units attribute")
+
+    def applies_to(self, node: Dataset | Group) -> bool:
+        names = [
+            'detector_number',
+            'detector_id',
+            'detector_index',
+            'event_id',
+            'event_index',
+        ]
+        name = node.name.split('/')[-1]
+        return isinstance(node, Dataset) and name in names
+
+    def validate(
+        self, tree: dict[str, Dataset | Group], node: Dataset | Group
+    ) -> Violation | None:
+        print(node.name)
+        if 'units' in node.attrs:
+            return Violation(node.name)
+
+
+class float_dataset_has_no_units(Validator):
+    def __init__(self) -> None:
+        super().__init__(
+            "float_dataset_has_no_units", "Float dataset should have units attribute"
+        )
+
+    def applies_to(self, node: Dataset | Group) -> bool:
+        return isinstance(node, Dataset)
+
+    def validate(
+        self, tree: dict[str, Dataset | Group], node: Dataset | Group
+    ) -> Violation | None:
+        if node.dtype in ['float32', 'float64'] and 'units' not in node.attrs:
+            return Violation(node.name)
+
+
+def is_transformation(node: Dataset | Group) -> bool:
+    return 'transformation_type' in node.attrs
+
+
+class missing_offset_units(Validator):
+    def __init__(self) -> None:
+        super().__init__("missing_offset_units", "Offset should have units attribute")
+
+    def applies_to(self, node: Dataset | Group) -> bool:
+        return isinstance(node, Dataset) and is_transformation(node)
+
+    def validate(
+        self, tree: dict[str, Dataset | Group], node: Dataset | Group
+    ) -> Violation | None:
+        if 'offset' in node.attrs:
+            if 'offset_units' not in node.attrs:
+                return Violation(node.name)
+
+
+class transformation_depends_on_missing(Validator):
+    def __init__(self) -> None:
+        super().__init__(
+            "transformation_missing_depends_on",
+            "Transformation should have depends_on attribute",
+        )
+
+    def applies_to(self, node: Dataset | Group) -> bool:
+        return isinstance(node, Dataset) and is_transformation(node)
+
+    def validate(
+        self, tree: dict[str, Dataset | Group], node: Dataset | Group
+    ) -> Violation | None:
+        if 'depends_on' not in node.attrs:
+            return Violation(node.name)
+
+
 def base_validators():
-    return [has_NX_class_attr(), depends_on_exists(), legacy_nexus_class()]
+    return [
+        NX_class_attr_missing(),
+        depends_on_target_missing(),
+        legacy_nexus_class(),
+        group_has_units(),
+        invalid_units(),
+        index_has_units(),
+        float_dataset_has_no_units(),
+        missing_offset_units(),
+        transformation_depends_on_missing(),
+    ]
